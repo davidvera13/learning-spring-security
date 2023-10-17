fea37bf229413c212ecf7ed3be1c9f5f6445a2a1
davidvera13 <david.vera@9online.fr>
2023-10-17 06:40:40 +0200

    feat(security): basic project setup w/o security

---
+++ Spring Security 6 Zero to Master along with JWT,OAUTH2/step01-basic-security/src/main/java/com/example/app/Step01BasicSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step01BasicSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step01BasicSecurityApplication.class, args);
+    }
+
+}
