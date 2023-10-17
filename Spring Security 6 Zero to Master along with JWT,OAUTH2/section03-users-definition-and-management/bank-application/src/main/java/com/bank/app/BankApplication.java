11f11efa32bf3b09d03c509200e68cae228fa30c
davidvera13 <david.vera@9online.fr>
2023-10-18 00:40:49 +0200

    feat(setup): users definition and management startup project

---
+++ Spring Security 6 Zero to Master along with JWT,OAUTH2/section03-users-definition-and-management/bank-application/src/main/java/com/bank/app/BankApplication.java
@@ -0,0 +1,18 @@
+package com.bank.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@SpringBootApplication
+public class BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+    /**
+
+     *
+     */
+
+}
