cec12243eeef2cf6fc7b813701ad98dad1ec4bfd
davidvera13 <david.vera@9online.fr>
2023-10-17 06:50:53 +0200

    feat(security): basic project with basic security

--- Spring Security 6 Zero to Master along with JWT,OAUTH2/step01-basic-security/src/main/java/com/example/app/controller/WelcomeController.java
+++ Spring Security 6 Zero to Master along with JWT,OAUTH2/step01-basic-security/src/main/java/com/example/app/controller/WelcomeController.java
@@ -3,10 +3,24 @@ package com.example.app.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest controller for test
+ * We must pass:
+ * <ul>
+ *     <li>user: user</li>
+ *     <li>password (example): 63aa414c-27fd-4fb7-8514-c21514601020</li>
+ *     <li>password is printed in console </li>
+ * </ul>
+ *
+ */
 @RestController
 public class WelcomeController {
+    /**
+     * sayHello method
+     * @return a string saying hello
+     */
     @GetMapping("/welcome")
     public String sayHello() {
-        return "Welcome to an unsecure application...";
+        return "Welcome to an secured application...";
     }
 }
